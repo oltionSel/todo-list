318183236d2d46085e1d14d3e8c2ac9fcb2d5662
Taulantibishi <taulant.ibishi2011@gmail.com>
2026-03-31 18:39:18 +0200

    olta

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include('partials/header.php');
+?>
+
+<div class="container" style="min-height:90vh">
+    
+      <h1 class="display-4 text-center mt-5">
+    Detyrat
+</h1>
+   
+</div>
+
+
+<?php
+include('footer.php');
+?>
