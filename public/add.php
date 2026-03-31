13e3e02fc735510f806f0dd53208d79c5e7dd8bc
oltiona132 <taulant.ibishi2011@gmail.com>
2026-03-31 19:00:37 +0200

    Olta2

---
+++ public/add.php
@@ -0,0 +1,50 @@
+<?php
+include('../partials/header.php');
+?>
+
+<div class="container-fluid bg-body-tertiary align-content-center" style="height:88.3vh">
+
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card shadow-lg p-3 rounded rounded-3">
+                <p class="h1 text-primary mb-3">Shto Detyra</p>
+                <form action="" method="post" enctype="multipart/form-data">
+
+                    <!-- Emri i detyres -->
+                    <div class="mb-3">
+                        <input type="text" class="form-control" id="emri_detyres" name="emri_detyres"
+                            placeholder="Shkruaj emrin e Detyres">
+                    </div>
+
+                    <!-- Data e fillimit  -->
+                    <div class="mb-3">
+                        <input type="date" class="form-control" id="data_fillimit" name="data_fillimit"
+                            placeholder="Shkruaj daten e fillimit">
+                    </div>
+
+                    <!-- Data e mbarimit  -->
+                    <div class="mb-3">
+                        <input type="date" class="form-control" id="data_mbarimit" name="data_mbarimit"
+                            placeholder="Shkruaj daten e mbarimit">
+                    </div>
+
+                    <!-- Statusi  -->
+                    <div class="mb-3">
+                         <!-- olti mtha bane koment SELECT -->
+                    </div>
+
+
+                    <div class="mb-3 w-100 d-flex justify-content-center align-content-center">
+                        <button class="btn btn-outline-primary w-50" type="submit">Ruaj Të Dhënat</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php
+include('/partials/footer.php');
+?>
---
+++ public/store.php
@@ -0,0 +1,40 @@
+<?php
+include('../classes/Task.php');
+include('../partials/header.php');
+
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+
+    $error=[];
+
+
+    // Emri i detyres
+    if(isset($_POST['emri_detyres']) && !empty($_POST['emri_detyres'])) {
+
+        $emri_detyres=htmlspecialchars(strip_tags(trim($_POST['emri_detyres'])));
+
+        if(strlen($emri_detyres)<2) {
+            $error[]="Emri i detyres duhet te perbehet nga se paku 3 karaktere!";
+        }
+
+    }
+    else {
+        $error[]="Ju lutemi te plotesoni fushen e emrit te detyres!";
+    }
+
+
+
+
+
+if(count($error)>0){
+foreach($error as $err){
+    echo"<p class='alert alert-danger p-3'>".$err."</p>";
+}
+
+}
+else{
+    $task=new Task();
+    $task->getAllTasks($emri);
+
+}
+}
